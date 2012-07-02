2dad71788c26a174cc8710f1e3fdf8f62216a15f
Zation <zation1@gmail.com>
2012-07-02 21:53:59 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chaocoolate::Application.config.secret_token = '64613e15c4c581959280d4d5ed6f63579db875ea636076fa1217f0c8840f97a7dee893832e32c8ad6663e9116213fe28abac8ad7c7a18274486a356cd715bf30'
